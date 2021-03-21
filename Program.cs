616287f0620e6d2c18d5963f02458773a42be5bb
Miriam Chen <45667250+Miriam410@users.noreply.github.com>
2021-03-20 22:27:22 -0300

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio5
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            char letra;         
+            do
+            {
+                Console.WriteLine("Ingrese las letras presionadas.");
+                letra = Convert.ToChar(Console.ReadLine());
+                Console.WriteLine("Para finalizar presione  Ctrl+F al mismo tiempo");
+            } while ();
+   
+        }
+    }
+}
